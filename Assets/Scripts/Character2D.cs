3822b5494f4369638013cd7c4f0d2733c383966e
jfcarocota <jfcarocota@gmail.com>
2020-01-16 07:54:13 -0700

    axis function and collision 2d

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed;
+    //float moveSpeed {get; set;}
+
     void Update()
     {
-        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
     }
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
 }
